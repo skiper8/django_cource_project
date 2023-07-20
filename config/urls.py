bb240b9527b8df1d30caa925b5423863df7a3823
askip <a.skiper2013@yandex.ru>
2023-07-20 17:53:31 +0300

    1

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ import blog
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', include('blog.urls', namespace='blog')),
+                  path('blog/', include('blog.urls', namespace='blog')),
                   path('', include('mailing.urls', namespace='mailing')),
                   path('users/', include('users.urls', namespace='users')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
